cb6a7b45fa16d2e2c2c89fea8029be264909fa47
Dicle Acet <71029563+dicleacet@users.noreply.github.com>
2023-02-13 18:39:12 +0300

    Set up the base template

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Home
   </div>
 </template>
 
@@ -11,8 +10,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  components: {  
   }
 }
 </script>
